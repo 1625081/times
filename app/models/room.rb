a6fb83b8f46efdf806d24435214303e6c48bb655
1625081 <sanjin@buaa.edu.cn>
2017-10-14 15:07:32 +0800

    renew

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,12 +1,14 @@
 class Room < ApplicationRecord
 
+  @@dict=Hash.new {'noinfo'}
+  @@dict={1=>"mon",2=>"tue",3=>"wed",4=>"thr",5=>"fri",6=>"mon",7=>"mon"}
+  @@dict2={"12"=>"上午","34"=>"下午","56"=>"晚上"}
 # 这是一种仅仅支持新建room的算法，不可以进行更新
 #  def self.import(file)
 #      CSV.foreach(file.path, headers: true) do |row|
 #	      Room.create! row.to_hash
 #	  end
 #  end
-
 #　本方法支持对有错误信息的room进行更新（通过新的CSV数据表）
   def self.import(file)
     CSV.foreach(file.path, headers: true) do |row|
@@ -15,6 +17,7 @@ class Room < ApplicationRecord
       room.save!
     end
   end
+
 
 # 用来确保一个教室在数据库唯一的方法
   def self.check()
@@ -38,17 +41,14 @@ class Room < ApplicationRecord
       # end
       # key2||=""
       # return key1,key2
-    if [6,0].include? Time.new.wday
-      return nil,nil
-    else
+    #  if [6,0].include? Time.new.wday
+    #    return nil,nil
+    #  else
       return "now",key
-    end
+    #  end
   end
 
   def match
-    dict=Hash.new {'noinfo'}
-    dict={1=>"mon",2=>"tue",3=>"wed",4=>"thr",5=>"fri"}
-    dict2={"12"=>"上午","34"=>"下午","56"=>"晚上"}
     h=Time.new.hour
     if h<12
       str="12"
@@ -60,23 +60,21 @@ class Room < ApplicationRecord
       str="56"
       #晚上
     end
-    if eval('self.'+dict[Time.new.wday]).nil?
+    if eval('self.'+@@dict[Time.new.wday]).nil?
       return "全天空闲"
-    elsif ((eval('self.'+dict[Time.new.wday]))=~/["#{str}"]/).nil?
-      return dict2["#{str}"]+"空闲"
-    elsif eval('self.'+dict[Time.new.wday]).include? str
-      return dict2["#{str}"]+"满课"
-    elsif eval('self.'+dict[Time.new.wday]).include? str[0]
+    elsif ((eval('self.'+@@dict[Time.new.wday]))=~/["#{str}"]/).nil?
+      return @@dict2["#{str}"]+"空闲"
+    elsif eval('self.'+@@dict[Time.new.wday]).include? str
+      return @@dict2["#{str}"]+"满课"
+    elsif eval('self.'+@@dict[Time.new.wday]).include? str[0]
       return "第"+str[1]+"节空闲"
-    elsif eval('self.'+dict[Time.new.wday]).include? str[1]
+    elsif eval('self.'+@@dict[Time.new.wday]).include? str[1]
       return "第"+str[0]+"节空闲"
     end
 
   end
 
   def self.resort(rooms)#按照时间点和教室id排序
-    dict=Hash.new {'noinfo'}
-    dict={1=>"mon",2=>"tue",3=>"wed",4=>"thr",5=>"fri"}
     h=Time.new.hour
     if h<12
       str="12"#早上
@@ -90,11 +88,11 @@ class Room < ApplicationRecord
     @room2=[]#有空闲教室
     @room3=[]#无空闲教室
     rooms.each do |r|
-      if eval('r.'+dict[Time.new.wday]).nil?
-        @room0<<r#上述代码周六日测试时会有bug
-      elsif ((eval('r.'+dict[Time.new.wday]))=~/["#{str}"]/).nil?
+      if eval('r.'+@@dict[Time.new.wday]).nil?
+        @room0<<r #上述代码周六日测试时会有bug
+      elsif ((eval('r.'+@@dict[Time.new.wday]))=~/["#{str}"]/).nil?
         @room1=@room1<<r
-      elsif eval('r.'+dict[Time.new.wday]).include? str
+      elsif eval('r.'+@@dict[Time.new.wday]).include? str
         @room3=@room3<<r
       else
         @room2<<r
@@ -108,8 +106,6 @@ class Room < ApplicationRecord
   end
 
   def self.search(mode,key)
-    dict=Hash.new {'noinfo'}
-    dict={1=>"mon",2=>"tue",3=>"wed",4=>"thr",5=>"fri"}
   	@rooms=[]
     @strs=[]
     # if (mode=~/周|星期/).is_a?(Fixnum)
